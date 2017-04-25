64403a25307f5f6c33703b7cbda147bb9f17a4cf
Cong Nguyen <cong.nguyen3@pcc.edu>
2017-04-24 19:07:56 -0700

    method that print out hell world!

--- src/cis233J/helloworld/helloworld.java
+++ src/cis233J/helloworld/helloworld.java
@@ -4,4 +4,7 @@ package cis233J.helloworld;
  * Created by user on 4/23/2017.
  */
 public class helloworld {
+    public static void main(String[] arg) {
+        System.out.println("Hello, World!");
+    }
 }
